831a3dda2b17dd90254ec3bd08589cb694b76dc1
Lain <tianshang360@163.com>
2021-10-10 01:10:31 +0800

    fix

--- src/components/admin/Author.vue
+++ src/components/admin/Author.vue
@@ -1,15 +1,17 @@
 <template>
-  <div style="height:700px;" v-infinite-scroll="load">
-    <el-card v-for="item in resultData.content" style="width: 300px;float: left;margin: 10px" :body-style="{ padding: '0px' }">
-      <div v-if="item.no_preview" @click="jumpToGallery(item._id)" style="text-align: center">
-        <h1>不可描述</h1>
-      </div>
-      <el-image v-else  @click="jumpToGallery(item._id)" style="width: 300px" fit="contain" :src="item.urls.regular"></el-image>
-      <div style="padding: 14px;">
-        <h3>画师：{{ item.author_name }}</h3>
-        <h3>UID:{{ item._id }}</h3>
-      </div>
-    </el-card>
+  <div style="height:700px;" >
+    <div class="masonry" v-infinite-scroll="load">
+      <el-card v-for="item in resultData.content" style="width: 100%;break-inside: avoid;margin-bottom: 10px" :body-style="{ padding: '0px' }">
+        <div v-if="item.no_preview" @click="jumpToGallery(item._id)" style="text-align: center">
+          <h1>不可描述</h1>
+        </div>
+        <el-image v-else  @click="jumpToGallery(item._id)" style="width: 100%" fit="contain" :src="item.urls.regular"></el-image>
+        <div style="padding: 14px;">
+          <h3>画师：{{ item.author_name }}</h3>
+          <h3>UID:{{ item._id }}</h3>
+        </div>
+      </el-card>
+    </div>
   </div>
 
 </template>
@@ -24,7 +26,7 @@ export default {
   data(){
     return{
       formData:{
-        pageSize:10,
+        pageSize:5,
         pageNum:1,
       },
       resultData:{
@@ -42,15 +44,15 @@ export default {
       });
       window.open(routeUrl .href, '_blank');
     },
-    async load() {
-      console.log("?")
+    load() {
       if (this.resultData.pageSize * this.resultData.pageNum <= this.resultData.total) {
         this.formData.pageNum = this.formData.pageNum + 1
-        const {data} = await fetchAuthors(this.formData)
-        this.resultData.content = this.resultData.content.concat(data.content)
-        this.resultData.pageNum = data.pageNum
-        this.resultData.pageSize = data.pageSize
-        console.log(this.resultData.content)
+        fetchAuthors(this.formData).then(e=>{
+          let data = e.data
+          this.resultData.content = this.resultData.content.concat(data.content)
+          this.resultData.pageNum = data.pageNum
+          this.resultData.pageSize = data.pageSize
+        })
       }
     },
     async fetchAuthorList(){
@@ -62,4 +64,11 @@ export default {
 </script>
 
 <style scoped>
+.masonry {
+  padding: 30px;
+  width: 1440px;
+  margin: 20px auto;
+  columns: 4;
+  column-gap: 30px;
+}
 </style>
